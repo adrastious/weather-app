0082fd73070b7ededcef7a546ae6496b281c321d
Carlos Del Angel <carlos@mitunetwork.com>
2021-10-10 18:31:16 -0500

    adding fix

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + '... It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain. ' + 'The atmospheric pressure is: ' + pressure)
+            callback(undefined, body.current.weather_descriptions + '... It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain. ' + 'The atmospheric pressure is: ' + body.current.pressure)
         }
     })
 }
